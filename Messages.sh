789c2efa21c7048d6be2590ddde60022463a5b00
George Florea Bănuș <georgefb899@gmail.com>
2022-11-06 14:57:42 +0200

    extract qml as javascript

--- Messages.sh
+++ Messages.sh
@@ -5,5 +5,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
+# ensure it is defined
+podir=${podir:?}
+
 $EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o $podir/haruna.pot
+$XGETTEXT $(find . -name \*.cpp -o -name \*.h) -o "$podir"/haruna.pot
+# Extract JavaScripty files as what they are, otherwise for example
+# template literals won't work correctly (by default we extract as C++).
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+$XGETTEXT --join-existing --language=JavaScript $(find . -name \*.qml -o -name \*.js) -o "$podir"/haruna.pot
+
