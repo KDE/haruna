ca9b67a58319a4652b226a1790afb917ac7ef581
George Florea Bănuș <georgefb899@gmail.com>
2023-05-08 22:05:58 +0300

    mpvabstractitem: stop listening to mpv events and call m_workerThread->wait()

--- src/mpv/mpvabstractitem.cpp
+++ src/mpv/mpvabstractitem.cpp
@@ -34,9 +34,11 @@ MpvAbstractItem::~MpvAbstractItem()
     if (m_mpv_gl) {
         mpv_render_context_free(m_mpv_gl);
     }
+    mpv_set_wakeup_callback(m_mpv, nullptr, nullptr);
     mpv_terminate_destroy(m_mpv);
 
     m_workerThread->quit();
+    m_workerThread->wait();
     m_workerThread->deleteLater();
     delete m_mpvController;
 }
