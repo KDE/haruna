f757f6989b34ca33ae04d4dcb5a3bd9e493fd29a
George Florea Bănuș <georgefb899@gmail.com>
2024-05-22 23:57:45 +0300

    add license to config-haruna.h.cmake file

--- config-haruna.h.cmake
+++ config-haruna.h.cmake
@@ -1,2 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2024 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: CC-BY-4.0
+ */
+
 /* Defines whether we are building with dbus enabled */
 #cmakedefine01 HAVE_DBUS
