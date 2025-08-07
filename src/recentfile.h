44f10136803d27b532d9921c458ae949434c6667
George Florea Bănuș <georgefb899@gmail.com>
2025-08-07 13:45:45 +0300

    recentfile: add license

--- src/recentfile.h
+++ src/recentfile.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef RECENTFILE_H
 #define RECENTFILE_H
 
