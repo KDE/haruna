4c43728bb0cfe6f4a5de73bc7cb59c969726aa6d
George Florea Bănuș <georgefb899@gmail.com>
2025-10-30 04:11:54 +0200

    systemutils: add license headers

--- src/utilities/systemutils.cpp
+++ src/utilities/systemutils.cpp
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #include "systemutils.h"
 
 #include <QFontDatabase>
