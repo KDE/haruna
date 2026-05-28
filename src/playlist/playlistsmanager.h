a4a367648f15301e03591fc2279c615041f04aa1
George Florea Bănuș <georgefb899@gmail.com>
2026-05-29 00:08:40 +0300

    playlistsmanager: add license header

--- src/playlist/playlistsmanager.h
+++ src/playlist/playlistsmanager.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2026 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef PLAYLISTSMANAGER_H
 #define PLAYLISTSMANAGER_H
 
