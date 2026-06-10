5124d47e764f2a45179f499c3aeaf15cc1d95e92
George Florea Bănuș <georgefb899@gmail.com>
2026-06-10 06:52:21 +0300

    commandlineoptions: add license

--- src/utilities/commandlineoptions.cpp
+++ src/utilities/commandlineoptions.cpp
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
 #include "commandlineoptions.h"
 
--- src/utilities/commandlineoptions.h
+++ src/utilities/commandlineoptions.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2026 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef COMMANDLINEOPTIONS_H
 #define COMMANDLINEOPTIONS_H
 
